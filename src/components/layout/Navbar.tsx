f1d508fa00d4b1b52b3fe6f25dbf655e6ba64561
Jahangir <jahangir80842@gmail.com>
2025-06-21 17:02:15 +0500

    Revert "Change name"
    This reverts commit 3debe767b875736de9228353327ae13bd943b407.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -47,7 +47,7 @@ const Navbar: React.FC = () => {
           className="flex items-center gap-2 cursor-pointer"
         >
           <Server className="text-primary-500" size={24} />
-          <span className="font-bold text-xl">Jahangir</span>
+          <span className="font-bold text-xl">Jahangir<span className="gradient-text">Alam</span></span>
         </Link>
 
         {/* Desktop Menu */}
@@ -69,7 +69,7 @@ const Navbar: React.FC = () => {
           <a 
             href="/resume.pdf" 
             className="btn-primary flex items-center gap-2"
-            download="Jahangir-Resume.pdf"
+            download="Jahangir-Alam-Resume.pdf"
             onClick={(e) => {
               // Remove the preventDefault and alert, letting the natural download happen
             }}
@@ -108,7 +108,7 @@ const Navbar: React.FC = () => {
                 onClick={closeMenu}
               >
                 <Server className="text-primary-500" size={24} />
-                <span className="font-bold text-xl">Jahangir</span>
+                <span className="font-bold text-xl">Jahangir<span className="gradient-text">Alam</span></span>
               </Link>
             </div>
           </div>
@@ -134,7 +134,7 @@ const Navbar: React.FC = () => {
                 <a 
                   href="/resume.pdf" 
                   className="btn-primary inline-flex items-center gap-2"
-                  download="Jahangir-Resume.pdf"
+                  download="Jahangir-Alam-Resume.pdf"
                   onClick={closeMenu}
                 >
                   <Download size={18} />
